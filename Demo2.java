57790a37a0b65079178f0fb23a76e3b52763face
yash <iammyash1133@gmail.com>
2021-06-01 16:10:15 +0530

    New changes to Demo2

--- Demo2.java
+++ Demo2.java
@@ -3,6 +3,8 @@ class Demo1
 	public static void main(String args[])
 	{
 		System.out.println("Java Demo Class 2");
+		System.out.println("Second line in Class 2");
+		System.out.println("Third line in Class 2");
 	}
 }
 
